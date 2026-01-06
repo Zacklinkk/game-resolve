eac7564a6bed52fd6185681741c4818586eb1155
Zacklinkk <zacklinkk@users.noreply.github.com>
2026-01-06 18:46:49 +0800

    fix: support cloudflare pages with environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/game-resolve/',
+      base: process.env.VITE_DEPLOY_TARGET === 'cloudflare' ? '/' : '/game-resolve/',
       server: {
         port: 3000,
         host: '0.0.0.0',
